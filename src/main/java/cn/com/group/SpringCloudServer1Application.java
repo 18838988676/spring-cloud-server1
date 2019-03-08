690d91f4d006d26ba09057a1f5ab160400130abb
cec <cec@cec-PC>
2019-03-08 17:25:54 +0800

    ribbon添加成功 ：访问http://localhost:9001/product/ribbon  即可通过产品，自动调用用户1或者用户2

--- src/main/java/cn/com/group/SpringCloudServer1Application.java
+++ src/main/java/cn/com/group/SpringCloudServer1Application.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class SpringCloudServer1Application {
-	//123实现微服务高可用注册中心三个注册中心123
+	//123实现微服务高可用注册中心三个注册中心123 顶端对其专用
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudServer1Application.class, args);
 	}
