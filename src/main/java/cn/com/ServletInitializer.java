58228ec1151542a7e2a5e557481d76e85fb832e6
18838988676@163.com <ww@ww-PC>
2019-03-10 21:55:13 +0800

    统一命名服务

--- src/main/java/cn/com/ServletInitializer.java
+++ src/main/java/cn/com/ServletInitializer.java
@@ -1,4 +1,4 @@
-package cn.com.group;
+package cn.com;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
--- src/main/java/cn/com/SpringCloudServer1Application.java
+++ src/main/java/cn/com/SpringCloudServer1Application.java
@@ -1,4 +1,4 @@
-package cn.com.group;
+package cn.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
